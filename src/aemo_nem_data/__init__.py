9f89a2e70657c284dcb26befcb81ed4e188b3751
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2024-07-03 11:16:54 +1000

    Changes to be committed: 	modified:   pyproject.toml 	new file:   src/aemo_nem_data/__init__.py

---
+++ src/aemo_nem_data/__init__.py
@@ -0,0 +1,11 @@
+from .constants import (
+    EndPoint,
+    BaseUrl,
+    REGIONS,
+    AUTH_ERROR_CODES
+)
+from .aemonem import (
+    AemoNemData,
+    ) 
+
+__all__ = ["EndPoint","BaseUrl","REGIONS","AUTH_ERROR_CODES","AemoNemData"]
