ce96f954de12ff0ee58a3fd538720dd0c6a83e2a
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2024-07-08 04:23:19 +1000

    Changes to be committed: 	modified:   pyproject.toml 	modified:   src/aemonemdata/aemonem.py

--- src/aemonemdata/aemonem.py
+++ src/aemonemdata/aemonem.py
@@ -217,7 +217,10 @@ class AemoNemData:
                 records_count = len(self._aemo_data_results["current_price"][region])
                 current_actual_prices = sum(item["price_kw"] for item in self._aemo_data_results["current_price"][region])
                 current_5min_price = (max(current_price_data["actual"][region], key=lambda x:x["settlement_date"]))["price_kw"]
-                current_30min_avg = round(current_actual_prices/records_count,4)
+                if records_count !=0 :
+                    current_30min_avg = round(current_actual_prices/records_count,4)
+                else:
+                    current_30min_avg = 0
                 current_30min_forecast = round((min(current_price_data["forecast"][region], key=lambda x:x["settlement_date"]))["price_kw"],4)
                 current_30min_estimated = round((current_actual_prices + current_30min_forecast*(6-records_count))/6,4)
                 current_cumulative_price = round((max(current_price_data["actual"][region], key=lambda x:x["settlement_date"]))["cumulative_price"],0)
